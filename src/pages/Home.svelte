8a8239e7b00b982345eda0971bd3ab5626b7dfe0
Shane Prendergast <shane.prendergast@sigma.se>
2021-01-01 21:02:34 +0000

    Fixes to tools page ready to deploy

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -9,6 +9,15 @@ console.log(items);
 
 <PageTitle title="Home" />
 
+<p>
+  A collection of tools for those pesky calculations. Made by
+  <a href="www.webknit.console.uk" class="underline">Webknit</a>
+  with
+  <a href="https://svelte.dev/" class="underline">Sevelte</a>
+  and everything else is handled by
+  <a href="https://vercel.com/" class="underline">Vecel</a>.
+</p>
+
 {#each items as item}
   <div class="mt-8">
     <h2 class="text-2xl font-extrabold text-gray-900 mt-4">{item.name}</h2>
